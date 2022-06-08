a86758eef55fad9f571b43293c7f9a40b9078d7b
yejif15 <76830397+yejif15@users.noreply.github.com>
2022-06-08 21:50:58 +0900

    Use block commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
