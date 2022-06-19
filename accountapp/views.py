932a7cb61ebfe89d7eab312c37d160fbc12a7ab7
yejif15 <76830397+yejif15@users.noreply.github.com>
2022-06-19 16:43:38 +0900

    Update view

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+    
