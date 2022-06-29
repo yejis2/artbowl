3fac973fd30365875204f08419d9a1d228fe3015
yejif15 <76830397+yejif15@users.noreply.github.com>
2022-06-29 23:42:09 +0900

    Login

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -7,5 +8,9 @@ app_name= "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LoginView.as_view(), name='logout'),
+
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- artbowl/settings.py
+++ artbowl/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -141,3 +143,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
