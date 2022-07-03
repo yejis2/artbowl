fb7f3bfefc4928ae38c7e2d7cc8971ffe065ade3
yejif15 <76830397+yejif15@users.noreply.github.com>
2022-07-03 19:49:09 +0900

    UpdateView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name= "accountapp"
 #accountapp = 127.0.0.1.8000/account/hello_world
@@ -13,5 +13,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
 ]
