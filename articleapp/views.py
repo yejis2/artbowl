bbf000862b4df18531210b074808e58221857b6f
yejif15 <76830397+yejif15@users.noreply.github.com>
2022-09-25 11:30:58 +0900

    Add Mobile responsive layout

--- articleapp/views.py
+++ articleapp/views.py
@@ -62,5 +62,5 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginate_by = 5
+    paginate_by = 8
 
